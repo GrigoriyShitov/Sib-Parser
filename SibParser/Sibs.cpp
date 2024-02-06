b1e1b8ab07d36f38b0ffdddd2ca73825d53db92a
GrigroiyShitov <grisha.shitob1@gmail.com>
2024-02-06 17:34:49 +0300

    Added SIT 3 message and visio doc

--- SibParser/Sibs.cpp
+++ SibParser/Sibs.cpp
@@ -1,153 +1,7 @@
 #include "Sibs.h"
 
-bool Sib1Pack::Parse(uint8_t* ptr)
-{
 
-	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
-	if (!(*ptr & 0xc0)) {// 0xc0 = 1100 0000 для определения 0 битмапы ( маска 0 0 x x x)
-		CellChannelDescr = 0;
-
-		*ptr = *ptr & 0x0f;//отсекам маску
-
-		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
-			for (int j = 0; j < 8;j++) {
-				bool set = *ptr & (1 << j);
-				if (set) {
-					int pos = (15 - i) * 8 + j + 1;
-					*ListPtr++ = pos;
-				}
-			}
-			ptr++;
-		}
-		/*for (int i = 0;i < 128;i++) {
-			if (ARFCNsList[i] != 0)
-			{
-				cout << dec << (int)ARFCNsList[i] << " ";
-			}
-		}
-		cout << endl;*/
-
-
-	}
-	else if (!(*ptr & 0x60))// 0x60 = 0110 0000 для определения 1024 range (маска 1 0 0 x x)
-	{
-		CellChannelDescr = 10;
-	}
-	else if (!(*ptr & 0x58))// 0x58 = 0101 1000 для определения 512 range (маска 1 0 1 0 0)
-	{
-		CellChannelDescr = 9;
-	}
-	else if (!(*ptr & 0x50))// 0x50 = 0101 0000 для определения 256 range (маска 1 0 1 0 1)
-	{
-		CellChannelDescr = 8;
-	}
-	else if (!(*ptr & 0x48))// 0x48 = 0100 1000 для определения 128 range (маска 1 0 1 1 0)
-	{
-		CellChannelDescr = 7;
-	}
-	else if (!(*ptr & 0x40))// 0x40 = 0100 0000 для определения variable bit map
-	{
-		CellChannelDescr = 1;
-	}
-
-
-	//Парсинг RACH Control Parametr
-	//cout << showbase << hex << (int)*ptr << endl;
-	bool res = RACHparse(RACH, ptr);
-	ACC = *++ptr<<8;
-	ACC += *++ptr;
-	//вывод инфы
-	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl << " Protocol Discirimator: " << (int)ProtDisc << " Skip Indicator: " << (int)SkipInd << endl << " Message Type: " << (int)MesType << endl << " Cell Channel Descriprion: " << (int)CellChannelDescr << endl << " ACC: " << (int)ACC << endl;
-	cout << "List of ARFCNS:";
-	/*for (int i = 0;i < 128;i++) {
-			if (ARFCNsList[i] != 0)
-			{
-				cout << dec << (int)ARFCNsList[i] << " ";
-			}
-		}
-		cout << endl;*/
-	do {
-		ListPtr--;
-		cout << dec << (int)*ListPtr << " ";
-	} while (ListPtr != &ARFCNsList[0]);
-	cout << endl;
-
-	for (int i = 0; i < 5;i++) {
-		cout << dec << (int)RACH[i] << " ";
-	}
-
-    return true;
-}
-
-bool Sib2Pack::Parse(uint8_t* ptr)
-{
-	// шестой и пятый биты текущего байта соответствуют значниям соответствующих полей (20 = 0010 0000 и 10 = 0001 0000)
-	EXTIND = (*ptr & 0x20)>> 4;
-	BAIND = (*ptr & 0x10)>> 4;
-
-	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
-	if (!(*ptr & 0xc0)) {// 0xc0 = 1100 0000 для определения 0 битмапы ( маска 0 0 x x x)
-
-		
-		*ptr = *ptr & 0x0f;//отсекам маску
-		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
-			for (int j = 0; j < 8;j++) {
-				bool set = *ptr & (1 << j);
-				if (set) {
-					int pos = (15 - i) * 8 + j + 1;
-					*ListPtr++ = pos;
-				}
-			}
-			ptr++;
-		}
-		/*for (int i = 0;i < 128;i++) {
-			if (ARFCNsList[i] != 0)
-			{
-				cout << dec << (int)ARFCNsList[i] << " ";
-			}
-		}
-		cout << endl;*/
-
-
-	}
-	else if (!(*ptr & 0x60))// 0x60 = 0110 0000 для определения 1024 range (маска 1 0 0 x x)
-	{
-	}
-	else if (!(*ptr & 0x58))// 0x58 = 0101 1000 для определения 512 range (маска 1 0 1 0 0)
-	{
-	}
-	else if (!(*ptr & 0x50))// 0x50 = 0101 0000 для определения 256 range (маска 1 0 1 0 1)
-	{
-	}
-	else if (!(*ptr & 0x48))// 0x48 = 0100 1000 для определения 128 range (маска 1 0 1 1 0)
-	{
-	}
-	else if (!(*ptr & 0x40))// 0x40 = 0100 0000 для определения variable bit map
-	{
-	}
-
-	//NCC один байт
-	NCC = *ptr++;
-
-	//Парсинг RACH Control Parametr
-	bool res = RACHparse(RACH, ptr);
-	ACC = *++ptr << 8;
-	ACC += *++ptr;
-
-	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl << " Protocol Discirimator: " << (int)ProtDisc << " Skip Indicator: " << (int)SkipInd << endl << " Message Type: " << (int)MesType << endl << " NCC: " << (int)NCC << endl << " EXT-IND: " << (int)EXTIND << endl << " BA-IND: " << (int)BAIND << endl << " ACC: " << (int)ACC << endl;
-	do {
-		ListPtr--;
-		cout << dec << (int)*ListPtr << " ";
-	} while (ListPtr != &BCCHFreqList[0]);
-	cout << endl;
-
-	for (int i = 0; i < 5;i++) {
-		cout << dec << (int)RACH[i] << " ";
-	}
-	return true;
-}
-
-bool RACHparse(uint8_t RACH[5], uint8_t* ptr)
+bool GSMTAPPACK::RACHparse(uint8_t RACH[5], uint8_t* ptr)
 {
 	switch (*ptr & 0xc0)//маска для определения Max Retransmissions(определяется по первым двум битам)
 	{//значение сопоставляется с таблицей значений
@@ -257,4 +111,397 @@ bool RACHparse(uint8_t RACH[5], uint8_t* ptr)
 		break;
 	}
 	return false;
+}
+//_________________________________________________________________________________________________________________________________
+bool Sib1Pack::Parse(uint8_t* ptr)
+{
+
+	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
+	if (!(*ptr & 0xc0)) {// 0xc0 = 1100 0000 для определения 0 битмапы ( маска 0 0 x x x)
+		CellChannelDescr = 0;
+
+		*ptr = *ptr & 0x0f;//отсекам маску
+
+		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
+			for (int j = 0; j < 8;j++) {
+				bool set = *ptr & (1 << j);
+				if (set) {
+					int pos = (15 - i) * 8 + j + 1;
+					*ListPtr++ = pos;
+				}
+			}
+			ptr++;
+		}
+		/*for (int i = 0;i < 128;i++) {
+			if (ARFCNsList[i] != 0)
+			{
+				cout << dec << (int)ARFCNsList[i] << " ";
+			}
+		}
+		cout << endl;*/
+
+
+	}
+	else if (!(*ptr & 0x60))// 0x60 = 0110 0000 для определения 1024 range (маска 1 0 0 x x)
+	{
+		CellChannelDescr = 10;
+	}
+	else if (!(*ptr & 0x58))// 0x58 = 0101 1000 для определения 512 range (маска 1 0 1 0 0)
+	{
+		CellChannelDescr = 9;
+	}
+	else if (!(*ptr & 0x50))// 0x50 = 0101 0000 для определения 256 range (маска 1 0 1 0 1)
+	{
+		CellChannelDescr = 8;
+	}
+	else if (!(*ptr & 0x48))// 0x48 = 0100 1000 для определения 128 range (маска 1 0 1 1 0)
+	{
+		CellChannelDescr = 7;
+	}
+	else if (!(*ptr & 0x40))// 0x40 = 0100 0000 для определения variable bit map
+	{
+		CellChannelDescr = 1;
+	}
+
+
+	//Парсинг RACH Control Parametr
+	//cout << showbase << hex << (int)*ptr << endl;
+	bool res = RACHparse(RACH, ptr);
+	ACC = *++ptr << 8;
+	ACC += *++ptr;
+	//вывод инфы
+	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl << " Protocol Discirimator: " << (int)ProtDisc << endl << " Skip Indicator: " << (int)SkipInd << endl << " Message Type: " << (int)MesType << endl << " Cell Channel Descriprion: " << (int)CellChannelDescr << endl << " ACC: " << (int)ACC << endl;
+	cout << "List of ARFCNS:";
+	/*for (int i = 0;i < 128;i++) {
+			if (ARFCNsList[i] != 0)
+			{
+				cout << dec << (int)ARFCNsList[i] << " ";
+			}
+		}
+		cout << endl;*/
+	do {
+		ListPtr--;
+		cout << dec << (int)*ListPtr << " ";
+	} while (ListPtr != &ARFCNsList[0]);
+	cout << endl;
+
+	for (int i = 0; i < 5;i++) {
+		cout << dec << (int)RACH[i] << " ";
+	}
+
+	return true;
+}
+
+bool Sib2Pack::Parse(uint8_t* ptr)
+{
+	// шестой и пятый биты текущего байта соответствуют значниям соответствующих полей (20 = 0010 0000 и 10 = 0001 0000)
+	EXTIND = (*ptr & 0x20) >> 4;
+	BAIND = (*ptr & 0x10) >> 4;
+
+	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
+	if (!(*ptr & 0xc0)) {// 0xc0 = 1100 0000 для определения 0 битмапы ( маска 0 0 x x x)
+
+
+		*ptr = *ptr & 0x0f;//отсекам маску
+		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
+			for (int j = 0; j < 8;j++) {
+				bool set = *ptr & (1 << j);
+				if (set) {
+					int pos = (15 - i) * 8 + j + 1;
+					*ListPtr++ = pos;
+				}
+			}
+			ptr++;
+		}
+		/*for (int i = 0;i < 128;i++) {
+			if (ARFCNsList[i] != 0)
+			{
+				cout << dec << (int)ARFCNsList[i] << " ";
+			}
+		}
+		cout << endl;*/
+
+
+	}
+	else if (!(*ptr & 0x60))// 0x60 = 0110 0000 для определения 1024 range (маска 1 0 0 x x)
+	{
+	}
+	else if (!(*ptr & 0x58))// 0x58 = 0101 1000 для определения 512 range (маска 1 0 1 0 0)
+	{
+	}
+	else if (!(*ptr & 0x50))// 0x50 = 0101 0000 для определения 256 range (маска 1 0 1 0 1)
+	{
+	}
+	else if (!(*ptr & 0x48))// 0x48 = 0100 1000 для определения 128 range (маска 1 0 1 1 0)
+	{
+	}
+	else if (!(*ptr & 0x40))// 0x40 = 0100 0000 для определения variable bit map
+	{
+	}
+
+	//NCC один байт
+	NCC = *ptr++;
+
+	//Парсинг RACH Control Parametr
+	bool res = RACHparse(RACH, ptr);
+	ACC = *++ptr << 8;
+	ACC += *++ptr;
+
+	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl << " Protocol Discirimator: " << (int)ProtDisc << endl << " Skip Indicator: " << (int)SkipInd << endl << " Message Type: " << (int)MesType << endl << " NCC: " << (int)NCC << endl << " EXT-IND: " << (int)EXTIND << endl << " BA-IND: " << (int)BAIND << endl << " ACC: " << (int)ACC << endl;
+	do {
+		ListPtr--;
+		cout << dec << (int)*ListPtr << " ";
+	} while (ListPtr != &BCCHFreqList[0]);
+	cout << endl;
+
+	for (int i = 0; i < 5;i++) {
+		cout << dec << (int)RACH[i] << " ";
+	}
+	return true;
+}
+
+bool Sib2bisPack::Parse(uint8_t* ptr)
+{
+	// шестой и пятый биты текущего байта соответствуют значниям соответствующих полей (20 = 0010 0000 и 10 = 0001 0000)
+	EXTIND = (*ptr & 0x20) >> 4;
+	BAIND = (*ptr & 0x10) >> 4;
+
+	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
+	if (!(*ptr & 0xc0)) {// 0xc0 = 1100 0000 для определения 0 битмапы ( маска 0 0 x x x)
+
+
+		*ptr = *ptr & 0x0f;//отсекам маску
+		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
+			for (int j = 0; j < 8;j++) {
+				bool set = *ptr & (1 << j);
+				if (set) {
+					int pos = (15 - i) * 8 + j + 1;
+					*ListPtr++ = pos;
+				}
+			}
+			ptr++;
+		}
+		/*for (int i = 0;i < 128;i++) {
+			if (ARFCNsList[i] != 0)
+			{
+				cout << dec << (int)ARFCNsList[i] << " ";
+			}
+		}
+		cout << endl;*/
+
+
+	}
+	else if (!(*ptr & 0x60))// 0x60 = 0110 0000 для определения 1024 range (маска 1 0 0 x x)
+	{
+	}
+	else if (!(*ptr & 0x58))// 0x58 = 0101 1000 для определения 512 range (маска 1 0 1 0 0)
+	{
+	}
+	else if (!(*ptr & 0x50))// 0x50 = 0101 0000 для определения 256 range (маска 1 0 1 0 1)
+	{
+	}
+	else if (!(*ptr & 0x48))// 0x48 = 0100 1000 для определения 128 range (маска 1 0 1 1 0)
+	{
+	}
+	else if (!(*ptr & 0x40))// 0x40 = 0100 0000 для определения variable bit map
+	{
+	}
+
+	//Парсинг RACH Control Parametr
+	bool res = RACHparse(RACH, ptr);
+	ACC = *++ptr << 8;
+	ACC += *++ptr;
+
+	Rest = *++ptr;
+
+	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl << " Protocol Discirimator: " << (int)ProtDisc << endl << " Skip Indicator: " << (int)SkipInd << endl << " Message Type: " << (int)MesType << endl << endl << " EXT-IND: " << (int)EXTIND << endl << " BA-IND: " << (int)BAIND << endl << " ACC: " << (int)ACC << endl << " Rest octet " << (int)Rest << endl;
+	do {
+		ListPtr--;
+		cout << dec << (int)*ListPtr << " ";
+	} while (ListPtr != &ExtendBCCHFreqList[0]);
+	cout << endl;
+
+	for (int i = 0; i < 5;i++) {
+		cout << dec << (int)RACH[i] << " ";
+	}
+
+	return false;
+}
+
+bool Sib2terPack::Parse(uint8_t* ptr)
+{
+	//шестой и седьмой бит текущево байта ссответствую значению Multiband reporting ( маска 0x60 = 0110 0000)
+	MultibandRep = (*ptr & 0x60) >> 4;
+
+	// пятый бит текущего байта соответствует значнию соответствующего поля (10 = 0001 0000)
+	BAIND = (*ptr & 0x10) >> 4;
+
+	//определение формата. Число в CellChannelDescr означает степень двойки (в случае если формат 1024,512,256 или 128 range)
+	//маска определения формата в Neighbour Cell Description 2 отличается от Neighbour Cell Description и CellChannelDescr
+	//Здесь формат определяется по 8,4,3,2 битам
+	if (!(*ptr & 0x80)) {// 0x80 = 1000 0000 для определения 0 битмапы ( маска 0 x x x . x x x x)
+
+		*ptr = *ptr & 0x0f;//отсекам маску
+		for (int i = 0; i < 16;i++) {//заполнение списка Cell Channel Description
+			for (int j = 0; j < 8;j++) {
+				bool set = *ptr & (1 << j);
+				if (set) {
+					int pos = (15 - i) * 8 + j + 1;
+					*ListPtr++ = pos;
+				}
+			}
+			ptr++;
+		}
+		/*for (int i = 0;i < 128;i++) {
+			if (ARFCNsList[i] != 0)
+			{
+				cout << dec << (int)ARFCNsList[i] << " ";
+			}
+		}
+		cout << endl;*/
+
+
+	}
+	else if (!(*ptr & 0x08))// 0x08 = 0000 1000 для определения 1024 range (маска 1 x x x . 0 x x x)
+	{
+	}
+	else if (!(*ptr & 0x06))// 0x06 = 0000 0110 для определения 512 range (маска 1 x x x 1 0 0 x)
+	{
+	}
+	else if (!(*ptr & 0x04))// 0x04 = 0000 0100 для определения 256 range (маска 1 x x x 1 0 1 x)
+	{
+	}
+	else if (!(*ptr & 0x02))// 0x02 = 0000 0010 для определения 128 range (маска 1 x x x 1 1 0 x)
+	{
+	}
+	else if (*ptr & 0x8e)// 0x8e = 1000 1110 для определения variable bit map
+	{
+	}
+
+
+	return true;
+}
+
+
+
+bool Sib3Pack::Parse(uint8_t* ptr)
+{
+	CID = *ptr++ << 8;//1 byte
+	CID += *ptr++;//2 byte
+
+	MCC[1] = (*ptr & 0xf0) >> 4;//3 byte
+	MCC[0] = *ptr++ & 0x0f;//3byte
+	MCC[2] = *ptr & 0x0f;//4byte
+
+	MNC[2] = (*ptr++ & 0xf0) >> 4;//4byte
+	MNC[1] = (*ptr & 0xf0) >> 4;//5byte
+	MNC[0] = *ptr++ & 0x0f;//5byte
+
+	LAC = *ptr++ << 8;//6 byte
+	LAC += *ptr++;//7 byte
+
+	ControlChannelDescr[0] = *ptr++;//если нужно будет - распарсить можно легко
+	ControlChannelDescr[1] = *ptr++;
+	ControlChannelDescr[2] = *ptr++;
+
+	CellOptions[0] = (*ptr & 0x80) >> 7;// DN-IND
+	CellOptions[1] = (*ptr & 0x40) >> 6;// PWRC
+	CellOptions[2] = (*ptr & 0x30) >> 4;// DTX
+	CellOptions[3] = (*ptr++ & 0x0f);//  RADIO-LINK-TIMEOUT
+
+	CellSelectParam[0] = (*ptr & 0xe0);// CELL-RESELECT HYSTERESIS
+	CellSelectParam[1] = (*ptr++ & 0x1f);// MS-TXPWR-MAX-CCH
+	CellSelectParam[2] = (*ptr & 0x80);// ACS
+	CellSelectParam[3] = (*ptr & 0x40);// NECI
+	CellSelectParam[4] = (*ptr++ & 0x3f);// RXLEV-ACCESS-MIN
+
+	bool res = RACHparse(RACH, ptr);
+	ACC = *++ptr << 8;
+	ACC += *++ptr;
+	ptr++;
+	Rest[0] = *ptr++;
+	Rest[1] = *ptr++;
+	Rest[2] = *ptr++;
+	Rest[3] = *ptr;
+	return true;
+}
+void Sib3Pack::printOutput()
+{
+	cout << showbase << hex << "L2PseudoL: " << (int)L2PseudoL << endl
+		<< " Protocol Discirimator: " << (int)ProtDisc << endl
+		<< " Skip Indicator: " << (int)SkipInd << endl
+		<< " Message Type: System Infortmation Type 3 " << (int)MesType << endl
+		<< " CID: " << dec << (int)CID << endl;
+
+	//right order of mcc:
+	cout << "MCC.MNC.LAC:";
+	for (uint8_t i : MCC) {
+		cout << dec << (int)i;
+	}
+	cout << ".";
+	for (uint8_t i : MNC) {
+		cout << dec << (int)i;
+	}
+	cout << "." << LAC << endl;
+
+	//3 октета Controll Channel Description
+	cout << " 3 octets of Controll Channel Description: ";
+	for (uint8_t i : ControlChannelDescr) {
+		cout << hex << (int)i << " ";
+	}
+	cout << endl;
+
+	for (int i = 0; i < 5;i++) {
+		switch (i)
+		{
+		case 0:
+			cout << " Maximum " << dec << (int)RACH[i] << " retransmissions";
+			break;
+		case 1:
+			cout << dec << (int)RACH[i] << " slots used to spread transmissions";
+			break;
+		case 2:
+			cout << dec << (int)RACH[i];
+			if (RACH[i])
+			{
+				cout << " The cell is barred";
+			}
+			else
+			{
+				cout << " The cell is not barred";
+			}
+
+			break;
+		case 3:
+			cout << dec << (int)RACH[i];
+			if (RACH[i])
+			{
+				cout << " Call Reestablishment not allowed in the cell ";
+			}
+			else
+			{
+				cout << " Call Reestablishment allowed in the cell ";
+			}
+
+			break;
+		case 4:
+			cout << dec << (int)RACH[i];
+			if (RACH[i])
+			{
+				cout << " Emergency call not allowed in the cell except for the MSs that belong to one of the classes between 11 to 15. ";
+			}
+			else
+			{
+				cout << " Emergency call allowed in the cell to all MSs  ";
+			}
+		default:
+			break;
+		}
+		cout << endl << " ";
+	}
+	cout << "ACC: " << hex << showbase << ACC << endl;
+	cout << "Rest 4 octets:";
+	for (uint8_t i : Rest) {
+		cout << (int)i << " ";
+	}
 }
--- SibParser/Sibs.h
+++ SibParser/Sibs.h
@@ -5,11 +5,19 @@ using namespace std;
 #include <stdint.h>
 #include <iostream>
 
+#define SYSTEM_INFORMATION_TYPE_1 0x19
+#define SYSTEM_INFORMATION_TYPE_2 0x1a
+#define SYSTEM_INFORMATION_TYPE_2bis 0x02
+#define SYSTEM_INFORMATION_TYPE_2ter 0x03
+#define SYSTEM_INFORMATION_TYPE_3 0x1b
+
+
 struct GSMTAPPACK {
 	uint8_t L2PseudoL;
 	uint8_t ProtDisc;
 	uint8_t SkipInd;
 	uint8_t MesType;
+	bool RACHparse(uint8_t RACH[5], uint8_t* ptr);
 };
 
 struct Sib1Pack : public GSMTAPPACK {
@@ -34,7 +42,39 @@ struct Sib2Pack : public GSMTAPPACK {
 	bool Parse(uint8_t* ptr);
 };
 
+struct Sib2bisPack : public GSMTAPPACK {
 
-bool RACHparse(uint8_t RACH[5], uint8_t* ptr);
+	uint8_t ExtendBCCHFreqList[128] = { 0 };
+	uint8_t* ListPtr = ExtendBCCHFreqList;
+	uint8_t EXTIND, BAIND;
+	uint8_t RACH[5] = { 0 };
+	uint16_t ACC;
+	uint8_t Rest;
+	bool Parse(uint8_t* ptr);
+};
+
+struct Sib2terPack : public GSMTAPPACK {
+
+	uint8_t ExtendBCCHFreqList[128] = { 0 };
+	uint8_t* ListPtr = ExtendBCCHFreqList;
+	uint8_t BAIND, MultibandRep;
+	uint8_t Rest;
+	bool Parse(uint8_t* ptr);
+};
+
+struct Sib3Pack : public GSMTAPPACK {
+	uint16_t CID;
+	uint8_t MCC[3] = { 0 };
+	uint8_t MNC[3] = { 0 };
+	uint16_t LAC;
+	uint8_t ControlChannelDescr[3] = { 0 };
+	uint8_t CellOptions[4] = { 0 };
+	uint8_t CellSelectParam[5] = { 0 };
+	uint8_t RACH[5] = { 0 };
+	uint16_t ACC;
+	uint8_t Rest[4] = { 0 };
+	void printOutput();
+	bool Parse(uint8_t* ptr);
+};
 
 #endif SIBS_H
